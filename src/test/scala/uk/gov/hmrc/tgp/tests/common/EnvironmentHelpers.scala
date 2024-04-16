38ec946aae4308e1a40813631fbae80deeafed9e
shiprashipra <shipra.shipra@digital.hmrc.gov.uk>
2024-04-16 12:12:21 +0100

    removed unused lines

--- src/test/scala/uk/gov/hmrc/tgp/tests/common/EnvironmentHelpers.scala
+++ src/test/scala/uk/gov/hmrc/tgp/tests/common/EnvironmentHelpers.scala
@@ -1,12 +1,5 @@
 package uk.gov.hmrc.tgp.tests.common
 
-import io.restassured.RestAssured.`given`
-import io.restassured.http.Cookies
-import io.restassured.path.json.JsonPath
-import io.restassured.response.Response
 
-import java.io.InputStream
-import scala.io.Source
-import scala.util.matching.Regex
 
 trait EnvironmentHelpers {}
