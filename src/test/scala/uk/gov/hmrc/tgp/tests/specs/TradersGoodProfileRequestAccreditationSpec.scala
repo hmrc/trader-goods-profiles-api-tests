0dff27ee084e31ccc9d42b280a2761fdd27f0a42
muthukumaran-palanisamy <152638509+muthukumaran-palanisamy@users.noreply.github.com>
2024-06-06 17:26:36 +0100

    TGP-850 DEFECT FIXES

--- src/test/scala/uk/gov/hmrc/tgp/tests/specs/TradersGoodProfileRequestAccreditationSpec.scala
+++ src/test/scala/uk/gov/hmrc/tgp/tests/specs/TradersGoodProfileRequestAccreditationSpec.scala
@@ -19,6 +19,7 @@ package uk.gov.hmrc.tgp.tests.specs
 import org.scalatest.Tag
 import uk.gov.hmrc.tgp.tests.client.HttpClient
 import uk.gov.hmrc.tgp.tests.utils.JsonUtils.getRequestJsonFileAsString
+import uk.gov.hmrc.tgp.tests.utils.JsonUtils.getResponseJsonFileAsString
 
 class TradersGoodProfileRequestAccreditationSpec extends BaseSpec with CommonSpec with HttpClient {
 
@@ -28,27 +29,31 @@ class TradersGoodProfileRequestAccreditationSpec extends BaseSpec with CommonSpe
 
     val scenarios = List(
       ("GB123456789011", 201, "201", "Validate success response 201 for Request Accreditation API call"),
-      // ("GB123456789012", 400, "Success", "Validate Invalid request parameter error message 400 for Request Accreditation API call"),
       (
         "GB123456789013",
         500,
         "Internal Error Response",
-        "Validate Internal Error Response 500 for Request Accreditation API call"
+        "Validate the error 'Internal Error Response' 500 for Request Accreditation API call"
       ),
-      ("GB123456789014", 500, "Unauthorized", "Validate the error Unauthorized 500 for Request Accreditation API call"),
+      (
+        "GB123456789014",
+        500,
+        "Unauthorized",
+        "Validate the error 'Unauthorized' 500 for Request Accreditation API call"
+      ),
       (
         "GB123456789015",
         500,
         "Internal Server Error",
-        "Validate internal server error response 500 for Request Accreditation API call"
+        "Validate the error 'internal server error' response 500 for Request Accreditation API call"
       ),
       (
         "GB123456789016",
         500,
         "Service Unavailable",
-        "Validate the error Service Unavailable 500 for Request Accreditation API call"
+        "Validate the error 'Service Unavailable' 500 for Request Accreditation API call"
       ),
-      ("GB123456789017", 500, "Bad Gateway", "Validate the error Bad Gateway 500 for Request Accreditation API call")
+      ("GB123456789017", 500, "Bad Gateway", "Validate the error 'Bad Gateway' 500 for Request Accreditation API call")
     )
 
     val FolderName = "RequestAccreditationAPI"
@@ -86,12 +91,26 @@ class TradersGoodProfileRequestAccreditationSpec extends BaseSpec with CommonSpe
 
     Scenario(s"Request Accreditation API - Validate Forbidden response 403 for Request Accreditation API") {
       val token      = givenGetToken(isValid = true, "GB123456789011")
-      val response   = requestAccreditation(token, "GB123456789001", PayloadWithoutActorId)
+      val response   = requestAccreditation(token, "GB123456789001", ValidPayload)
       val statusCode = response.getStatusCode
       System.out.println("Status code: " + statusCode)
       statusCode.shouldBe(403)
       val actualResponse = response.getBody.asString()
       assert(actualResponse.contains("EORI number is incorrect"))
+    }
+
+    Scenario(
+      s"Request Accreditation API - Validate response 400 with multiple error message for Request Accreditation API"
+    ) {
+      val token      = givenGetToken(isValid = true, "GB123456789012")
+      val response   = requestAccreditation(token, "GB123456789012", ValidPayload)
+      val statusCode = response.getStatusCode
+      System.out.println("Status code: " + statusCode)
+      statusCode.shouldBe(400)
+      val actualResponse   = response.getBody.asString()
+      System.out.println("Status code: " + actualResponse)
+      val expectedResponse = getResponseJsonFileAsString(FolderName, "Scenario_Create_400")
+      assert(compareJson(actualResponse, expectedResponse), "JSON response doesn't match the expected response.")
 
     }
 
